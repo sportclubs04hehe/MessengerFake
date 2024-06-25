cb1fc8105a79842cc956c38ac18956e6eabcc879
Le Minh Huy <vegakinvietnam@gmail.com>
2024-06-25 15:04:12 +0700

    FallbackController

---
+++ MessengerFake.API/Controllers/FallbackController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerFake.API.Controllers
+{
+    public class FallbackController : BaseApiController
+    {
+        public ActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot", "index.html"), "text/HTML");
+        }
+    }
+}
