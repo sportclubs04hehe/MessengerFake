5780b8491c5cb44a34637aedf3a81a204ba9b06f
Le Minh Huy <vegakinvietnam@gmail.com>
2024-06-25 14:55:56 +0700

    BuggyController

---
+++ MessengerFake.API/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+﻿using MessengerFake.API.Data;
+using MessengerFake.API.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerFake.API.Controllers
+{
+    public class BuggyController(DataContext _context) : BaseApiController
+    {
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetAuth()
+        {
+            return "văn bản bí mật";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if (thing == null) return NotFound();
+
+            return thing;
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<AppUser> GetServerError()
+        {
+            var thing = _context.Users.Find(-1) ?? throw new Exception("A bad thing has happened");
+
+            return thing;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
