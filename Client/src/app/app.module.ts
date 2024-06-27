e6e01f5a7303a7e0bdf188e1fb400368ffc30264
Le Minh Huy <vegakinvietnam@gmail.com>
2024-06-27 09:26:55 +0700

    Loại bỏ angular 16 để chuẩn bị cài đặt angular 17

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ Client/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7176/api/',
+  hubsUrl: 'https://localhost:7176/hubs/',
+};
